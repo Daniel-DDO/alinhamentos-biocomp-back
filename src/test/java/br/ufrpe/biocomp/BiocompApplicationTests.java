4f31bd041e187a33c211f7b0d8dd148089150c63
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2026-06-19 14:57:35 -0300

    Base SPRING

---
+++ src/test/java/br/ufrpe/biocomp/BiocompApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrpe.biocomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiocompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
